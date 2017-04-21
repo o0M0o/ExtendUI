4e17eb97c405ad9e2f9e0ceba5177bcb9e1dea54
hugo <fly_in_wind@qq.com>
2017-04-21 10:35:59 +0800

    first

---
+++ app/src/main/java/wxm/extendui/ACStart.java
@@ -0,0 +1,13 @@
+package wxm.extendui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ACStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_start);
+    }
+}
