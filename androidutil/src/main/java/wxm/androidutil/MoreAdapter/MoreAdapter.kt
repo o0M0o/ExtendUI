68032e464c27dbf366e0769420c7920878691be8
hugo <fly_in_wind@qq.com>
2018-05-17 19:36:39 +0800

    just record

--- androidutil/src/main/java/wxm/androidutil/MoreAdapter/MoreAdapter.kt
+++ androidutil/src/main/java/wxm/androidutil/MoreAdapter/MoreAdapter.kt
@@ -1,12 +1,13 @@
 package wxm.androidutil.MoreAdapter
 
 import android.content.Context
-import android.support.annotation.IdRes
 import android.support.annotation.LayoutRes
-import android.util.SparseArray
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Checkable
+import android.widget.ImageView
 import android.widget.SimpleAdapter
+import android.widget.TextView
 
 import wxm.androidutil.ViewHolder.ViewHolder
 
@@ -18,10 +19,49 @@ import wxm.androidutil.ViewHolder.ViewHolder
 abstract class MoreAdapter(protected val context: Context, data: List<Map<String, *>>,
                            @Suppress("MemberVisibilityCanBePrivate") @param:LayoutRes @field:LayoutRes
                            protected val mLRSelfDef: Int,
-                           from: Array<String?> = arrayOfNulls(0),
-                           to: IntArray = IntArray(0))
-    : SimpleAdapter(context, data, mLRSelfDef, from, to) {
+                           private val mFromKey: Array<String?> = arrayOfNulls(0),
+                           private val mToId: IntArray = IntArray(0))
+    : SimpleAdapter(context, data, mLRSelfDef, mFromKey, mToId) {
     private val mVWChild : Array<View?> = arrayOfNulls(data.size)
+    private val mDefaultViewBinder = ViewBinder { v, d, text ->
+        when (v) {
+            is Checkable -> {
+                when {
+                    d is Boolean -> {
+                        (v as Checkable).isChecked = d
+                    }
+                    v is TextView -> {
+                        setViewText(v as TextView, text)
+                    }
+                    else -> {
+                        throw IllegalStateException("${v.javaClass.name} should be bound to a Boolean, " +
+                                "not a ${d?.javaClass ?: "<unknown type>"}")
+                    }
+                }
+            }
+
+            is TextView -> {
+                // Note: keep the instanceof TextView check at the bottom of these
+                // ifs since a lot of views are TextViews (e.g. CheckBoxes).
+                setViewText(v, text)
+            }
+
+            is ImageView -> {
+                if (d is Int) {
+                    setViewImage(v, d)
+                } else {
+                    setViewImage(v, text)
+                }
+            }
+
+            else -> {
+                throw IllegalStateException("${v.javaClass.name} should be bound to a Boolean, " +
+                        "not a ${d?.javaClass ?: "<unknown type>"}")
+            }
+        }
+
+        true
+    }
 
     override fun getViewTypeCount(): Int {
         return count.let { if (it < 1) 1 else it }
@@ -35,6 +75,7 @@ abstract class MoreAdapter(protected val context: Context, data: List<Map<String
         return ViewHolder.get(context, convertView, getChildViewLayout(position)).let {
             loadView(position, it)
             mVWChild[position] = it.convertView
+            selfBindView(position, it.convertView)
 
             it.convertView
         }
@@ -62,6 +103,31 @@ abstract class MoreAdapter(protected val context: Context, data: List<Map<String
     @LayoutRes
     protected open fun getChildViewLayout(pos: Int): Int {
         return mLRSelfDef
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun selfBindView(position: Int, view: View): Boolean {
+        val bind = viewBinder
+        val map = getItem(position) as Map<String, *>
+        for(i in 0 until mToId.size)    {
+            view.findViewById<View>(mToId[i])?.let {
+                val data = map[mFromKey[i]]
+                val txt = data?.toString() ?: ""
+
+                var bound = false
+                if(null != bind) {
+                    bound = bind.setViewValue(it, data, txt)
+                }
+
+                if(!bound)  {
+                    mDefaultViewBinder.setViewValue(it, data, txt)
+                }
+
+                Unit
+            }
+        }
+
+        return true
     }
 
     /**
